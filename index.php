bc45fa08d35f2d0f3b566bd70dd185e156be9503
hursey013 <hursey013@gmail.com>
2015-12-07 15:49:47 -0500

    added google analytics

--- index.php
+++ index.php
@@ -162,5 +162,13 @@ if (file_exists($filename)) {
 		var hashvalue = "<?php if(isset($_POST["hashvalue"])){ echo $_POST["hashvalue"]; } ?>";
 	</script>
 	<script src="scripts.js"></script>
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	  ga('create', 'UA-69230191-1', 'auto');
+	  ga('send', 'pageview');
+	</script>
 </body>
 </html>
